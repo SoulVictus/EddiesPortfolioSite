001f08257082ca69d8e6c12c46a687e7838ffc4c
Slvi <jakub.lorek44@gmail.com>
2021-06-01 13:22:27 +0200

    init layout completed

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="row">
+          <h1>Strona</h1>
+        </div>
+      </header>
+      <div className="App-main">
+        <div className="content-container">
+          test
+        </div>
+        <div className="middle-container">
+          <div className="avatar-container">
+            <div className="avatar">
+
+            </div>
+          </div>
+          <div className="separator-container">
+            <div className="separator"></div>
+          </div>
+        </div>
+        <div className="content-container">
+          test2
+        </div>
+      </div>
+      <footer className="App-footer">
+      </footer>
+    </div>
+  );
+}
+
+export default App;
