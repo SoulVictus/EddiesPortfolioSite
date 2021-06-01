ec869b2d734c4647feb928ef2c75b9db82708c94
Slvi <jakub.lorek44@gmail.com>
2021-06-01 20:22:49 +0200

    added decorative component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Avatar from "./components/Avatar";
 import Title from "./components/Title";
 import TextArea from "./components/TextArea";
 import HorizontalSeparator from "./components/HorizontalSeparator";
+import Decorative from "./components/Decorative";
 
 function App() {
   return (
@@ -19,9 +20,7 @@ function App() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
               </TextArea>
               <HorizontalSeparator />
-              <TextArea>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-              </TextArea>
+              <Decorative />
             </div>
           </div>
         </div>
---
+++ src/components/Decorative.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Decorative.css";
+
+function Decorative() {
+    return (
+        <div className="decorative-container">
+            <div className="decorative-img">
+            </div>
+        </div>
+    )
+}
+
+export default Decorative
